af7a4de0611277d6944dcadf13de31ea2b2febe1
CourtMatt <courtney.matthews01@testpro.io>
2023-06-26 17:45:23 -0500

    import
    import chrome options

---
+++ src/test/java/HomeWork16.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HomeWork16 extends BaseTest {
+    @Test
+    public void registrationNavigation() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement registrationButton= driver.findElement(By.cssSelector("[id='hel']"));
+        registrationButton.click();
+
+        String expectedUrl = "https://qa.koel.app/register";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "Redirection to Registration page failed.");
+
+        driver.quit();
+    }
+}
